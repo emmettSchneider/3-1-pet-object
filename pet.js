d88ee4415511e532945187afdd48c6b9486449b5
Emmett Schneider <emmett.f.schneider@gmail.com>
2018-11-28 15:51:45 -0600

    adding completed files

---
+++ pet.js
@@ -0,0 +1,14 @@
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+const pet = {
+    name: "Calliope",
+    species: "canis canis",
+    nicknames: ["Cally Wally", "Callidopus", "devil dog"],
+    age: 1
+}
+console.log(pet);
